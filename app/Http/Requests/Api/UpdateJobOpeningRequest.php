57bc4a143197c76547ead8024ec157351538e096
csagastegui59 <csagastegui59@gmail.com>
2022-12-16 17:43:36 -0500

    Add update job opening request

---
+++ app/Http/Requests/Api/UpdateJobOpeningRequest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Requests\Api;
+
+use App\Http\Requests\ApiFormRequest;
+
+class UpdateJobOpeningRequest extends ApiFormRequest
+{
+    public function authorize()
+    {
+        $user = $this->user();
+
+        return $user != null && $user->role  ? true : false;
+    }
+
+    public function rules()
+    {
+        return [
+            'title' => ['sometimes', 'required', 'max:255'],
+            'description' => ['sometimes', 'required', 'max:2000'],
+            'is_published' => ['sometimes', 'required', 'boolean']
+        ];
+    }
+}

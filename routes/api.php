6739ba6ce7ae48d3e2bccba4280189d6b45c81d8
csagastegui59 <csagastegui59@gmail.com>
2022-12-16 12:16:04 -0500

    Add routes

--- routes/api.php
+++ routes/api.php
@@ -2,18 +2,51 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\{ 
+    AuthController,
+    CompanyController, 
+    JobOpeningController,
+    ApplicantController,
+    CandidateController,
+    UserController
+};
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::post('register', [AuthController::class, 'store']);
+Route::post('login', [ AuthController::class, 'login']);
+Route::resource('companies.jobOpenings.applicants', ApplicantController::class)
+    ->only('store');
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware(['auth:sanctum'])->group(function() {
+    Route::resource('companies.jobOpenings.applicants', ApplicantController::class)
+        ->only('index', 'show', 'destroy')
+        ->middleware('ability:api:admin, company:admin, company:recruiter');
+
+    Route::resource('companies.jobOpenings.candidates', CandidateController::class)
+        ->only('index', 'store', 'show', 'update', 'destroy')
+        ->middleware('ability:api:admin, company:admin, company:recruiter');
+
+    Route::resource('companies', CompanyController::class)
+        ->only('show', 'index')
+        ->middleware('ability:company:recruiter, api:admin, company:admin');
+
+    Route::resource('companies', CompanyController::class)
+        ->only('store', 'destroy')
+        ->middleware('ability:api:admin');
+
+    Route::resource('companies', CompanyController::class)
+        ->only('update')
+        ->middleware('ability:api:admin, company:admin');
+
+    Route::resource('companies.jobOpenings', JobOpeningController::class)
+        ->only('index', 'store', 'show', 'update', 'destroy')
+        ->middleware('ability:api:admin, company:admin, company:recruiter');
+
+    Route::resource('companies.users', UserController::class)
+        ->only('index', 'store', 'show', 'update', 'destroy')
+        ->middleware('ability:api:admin, company:admin, company:recruiter');
+    
+    Route::get('companies/{company}/jobOpenings/{jobOpening}/reports',[JobOpeningController::class, 'reports'])
+        ->middleware('ability:api:admin, company:admin, company:recruiter');
+
+    Route::post('logout', [AuthController::class, 'logout']);
 });
